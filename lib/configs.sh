ab645d43930ef749f35fe37d8b2759aaf9547957
Yoshi Nakamoto <yoshi@nkmt.to>
2025-08-02 18:41:19 +0800

    Added scaffolding for main bin script, configs, globals and functions.

---
+++ lib/configs.sh
@@ -0,0 +1,16 @@
+# Scan configs
+JUNJO_SCAN_DIR="./"
+JUNJO_SCAN_RECURSIVE=0
+JUNJO_IGNORE_FILES='*.json
+*.txt
+*.log'
+
+# Sort configs
+JUNJO_OUTPUT_DIR="./output"
+JUNJO_SORT_PLAN_FILE="junjo_sort_plan.map"
+
+# Log configs
+JUNJO_LOG_VERBOSE=0
+JUNJO_LOG_DIR="./"
+JUNJO_SCAN_LOG_FILE="junjo_scan.log"
+JUNJO_SORT_LOG_FILE="junjo_sort.log"
---
+++ lib/globals.sh
@@ -0,0 +1,58 @@
+# Properties of each file keyed by file id (fid)
+declare -A file_src
+declare -A file_src_dir
+declare -A file_src_name
+declare -A file_src_stem
+declare -A file_src_full_ext
+declare -A file_src_ext
+declare -A file_src_clean_stem
+declare -A file_dest
+declare -A file_dest_dir
+declare -A file_dest_name
+declare -A file_dest_stem
+declare -A file_dest_full_ext
+declare -A file_dest_ext
+declare -A file_type # See file_types for possible values
+declare -A file_cid
+declare -A file_make
+declare -A file_model
+declare -A file_timestamp
+declare -A file_timestamp_source
+declare -A file_device_name
+declare -A file_device_type
+declare -A file_device_folder
+declare -A file_device_origin
+declare -A file_takeout_json_file
+declare -A file_takeout_json_file_name
+declare -A file_takeout_json_file_match_strategy
+declare -A file_has_duplicates
+
+# File types enumerations
+declare -A file_types=(
+  ["AP"]="Apple Photo"
+  ["AV"]="Apple Video"
+  ["LP"]="Apple Live Photo"
+  ["LV"]="Apple Live Video"
+  ["RP"]="Regular Photo"
+  ["RV"]="Regular Video"
+  ["RI"]="Regular Image"
+  ["SS"]="Screenshot"
+  ["SR"]="Screen Recording"
+  ["?"]="Unknown"
+)
+
+# List of files by file type keyed by fid
+declare -A apple_photo_files
+declare -A apple_video_files
+declare -A live_photo_files
+declare -A live_video_files
+declare -A regular_photo_files
+declare -A regular_video_files
+
+# List of live photo and video fids keyed by cid
+declare -A live_photo_by_cid
+declare -A live_video_by_cid
+declare -A live_photo_dupes
+declare -A live_video_dupes
+declare -A live_photo_missing_video
+declare -A live_video_missing_photo
