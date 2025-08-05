c98c1e629b61401817a9e163337ac506057fe412
Yoshi Nakamoto <yoshi@nkmt.to>
2025-08-05 20:38:55 +0800

    Added main controller.

---
+++ lib/controller.sh
@@ -0,0 +1,11 @@
+junjo_start() {
+
+  # Step 1: Scan media folder
+  scan_media_folder
+
+  # Step 2: Create a plan to organize the media files
+  # create_organizing_plan
+
+  # Step 3: Sort media files
+  # sort_media_files
+}
